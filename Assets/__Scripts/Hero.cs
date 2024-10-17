49eec611938f5e0f9b7b4068c5ea794c8e51414a
Evan Bohler <57956682+ebohler@users.noreply.github.com>
2024-10-17 01:24:29 -0500

    game over and restart

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -13,8 +13,10 @@ public class Hero : MonoBehaviour
     public float rollMult = -45;
     public float pitchMult = 30;
 
-    [Header("Dynamic")] [Range(0,4)]
-    public float shieldLevel = 1;
+    [Header("Dynamic")] [Range(0,4)] [SerializeField]
+    private float _shieldLevel = 1;
+    [Tooltip("This field holds a reference to the last triggering GameObject")]
+    private GameObject lastTriggerGo = null;
 
     void Awake() {
         if (S == null) {
@@ -37,5 +39,31 @@ public class Hero : MonoBehaviour
 
         // Rotate the ship to make it feel more dynamic
         transform.rotation = Quaternion.Euler(vAxis*pitchMult,hAxis*rollMult,0);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+        
+        if (go == lastTriggerGo) return;
+        lastTriggerGo = go;
+
+        Enemy enemy = go.GetComponent<Enemy>();
+        if (enemy != null) {
+            shieldLevel--;
+            Destroy(go);
+        } else {
+            Debug.LogWarning("Shield trigger hit by non-Enemy"+go.name);
+        }
+    }
+
+    public float shieldLevel {
+        get {return(_shieldLevel);}
+        private set {
+            _shieldLevel = Mathf.Min(value,4);
+            if (value < 0) {
+                Destroy(this.gameObject);
+            }
+        }
     }
 }
--- Assets/__Scripts/Main.cs
+++ Assets/__Scripts/Main.cs
@@ -11,6 +11,7 @@ public class Main : MonoBehaviour
     public GameObject[] prefabEnemies;
     public float enemySpawnPerSecond = 0.5f;
     public float enemyInsetDefault = 1.5f;
+    public float gameRestartDelay = 2;
 
     private BoundsCheck bndCheck;
 
@@ -39,5 +40,13 @@ public class Main : MonoBehaviour
         go.transform.position = pos;
 
         Invoke(nameof(SpawnEnemy), 1f/enemySpawnPerSecond);
+    }
+
+    void DelayedRestart() {
+        Invoke(nameof(Restart), gameRestartDelay);
+    }
+
+    void Restart() {
+        SceneManager.LoadScene("__Scene_0");
     }
 }
